a9e72e97432e0e76c5b9afa166bf3c50ac186560
AlexKhodus <ali.khodus@yandex.ru>
2021-10-06 01:13:46 +0300

    Добавьте файлы проекта.

---
+++ TasksList/Models/TodoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksList.Models
+{
+    class TodoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private bool _isDone;
+        private string _textTodo;
+
+        public string TextTodo
+        {
+            get { return _textTodo; }
+            set { _textTodo = value; }
+        }
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+    }
+}
